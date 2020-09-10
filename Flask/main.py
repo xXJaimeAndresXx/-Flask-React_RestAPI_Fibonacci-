eaf6076d164697e7a87deb81d81466a14965fc32
Jaime Andres <jaimeandres2998@gmail.com>
2020-09-10 11:00:45 -0500

    Flask

---
+++ Flask/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/fibonacci', methods=['GET','POST'])
+def fibonacci():
+    number= request.args.get('number')
+    a, b = 0,1
+    serie=[]
+    while a < int(number):
+        print(a, end=' ')
+        serie.append(a)
+        a, b = b, a + b
+    json_serie=json.dumps(serie)
+     
+    return (json_serie)
+
+if __name__ == '__main__':
+    app.run(debug=True,port= 5000)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export const App = () => {
+  const [value, setValue] = useState("")
+  const [state, setState] = useState({ loading: true, data: null, error: null })
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5000/fibonacci?number=${value}`)
+    .then((response) => response.json()).then((data) => {
+      if (data) {
+        setState({ ...state, loading: false, data: data })
+      }
+    }).catch((e) => setState({ loaging: false, data: null, error: e.message }))
+
+  }, [value])
+
+  console.log(state.data)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    /*fetch(`http://127.0.0.1:5000/fibonacci`, {
+      method: "POST   ",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+
+      },
+
+      //make sure to serialize your JSON body
+      body: JSON.stringify({
+        value
+      })
+    })*/
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container ">
+      <div className="col-sm-12 my-auto ">
+        <p className="col-12">Fibonacci</p>
+      </div>
+      <form onSubmit={e => handleSubmit(e)} >
+        <div className="form-group">
+          <div className="col-12 my-auto ">
+            <label >Fibonacci Limit</label>
+            <input type="number" className="col-12" name="number" placeholder="Enter a Number" value={value} onChange={(e) => {
+              setValue(e.target.value)
+            }} />
 
-export default App;
+            <small className="form-text text-muted">Choose a number.</small>
+
+          </div>
+        </div>
+      </form>
+      <ul>{state.data && state.data.map((value, index) => (
+        <li key={index}>{value}</li>
+      ))}</ul>
+    </div>
+
+  )
+
+}
